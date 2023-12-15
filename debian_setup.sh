9818ff6f7e0c8b9350caab5b4c18f2ed97333ff0
AndrExAgris <andredecampos.ac@gmail.com>
2023-12-15 19:33:00 -0300

    v1 do deb pronta

--- debian_setup.sh
+++ debian_setup.sh
@@ -1,6 +1,36 @@
 #em produção
-#script pra debian com gonome
+#script pra debian com gnome
 
+### Atualizaçoes/Instalações do sistema
+
+sudo apt update && sudo apt upgrade -y
+
+#pra codar
+sudo apt install build-essential gcc vim git cmake zsh -y
+
+#util
+sudo apt install wget curl nmap net-tools ncdu btop openjdk-17-jdk vlc pkg-config -y
+
+#python
+sudo apt install python3 python3-pip python3-numpy python3-dev -y
+
+#customizacao
+sudo apt install gnome-shell-extension-manager -y
+
+#meme
+sudo apt install neofetch sl lolcat tldr cowsay -y
+
+#snaps
+sudo apt install snapd
+sudo systemctl enable --now snapd.socket
+
+#snaps --classic
+sudo snap install codium --classic  
+
+#snaps
+sudo snap install discord onlyoffice-desktopeditors
+
+#--------------------------------------------------------------------
 ### Configuracoes do sistema:
 
 #dark mode
@@ -14,46 +44,30 @@ gsettings set org.gnome.desktop.session idle-delay 0
 #som do sistema desabilitado
 gsettings set org.gnome.desktop.sound event-sounds false
 
+#desabilita animação
+gsettings set org.gnome.desktop.interface enable-animations false
+
 # Fontes
-gsettings set org.gnome.desktop.interface font-name 'Arial 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'DejaVu Sans Mono 11'
+cd Downloads/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
+unzip Hack.zip -d MyFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Ubuntu.zip
+unzip Ubuntu.zip -d MyFonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/UbuntuMono.zip
+unzip UbuntuMono.zip -d MyFonts
+sudo cp -r MyFonts /usr/share/fonts/truetype/
+sudo fc-cache -f -v
+gsettings set org.gnome.desktop.interface font-name 'Ubuntu Nerd Font Regular 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Hack Nerd Font 12'
+gsettings set org.gnome.desktop.interface monospace-font-name 'UbuntuMono Nerd Font Regular 13'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Ubuntu Nerd Font Bold 11'
 
 
-
-
-sudo apt update && sudo apt upgrade -y
-
-#snaps --classic
-sudo snap install code --classic  
-
-#snaps
-sudo snap install discord onlyoffice-desktopeditors
-
-#python
-sudo apt install python3-pip python3-numpy python3-dev -y
-
-#customizacao
-sudo apt install font-manager gnome-tweaks gnome-shell-extension-manager -y
-
-#meme
-sudo apt install neofetch sl lolcat tldr cowsay -y
-pip install git+https://github.com/will8211/unimatrix.git
-
-#util
-sudo apt install wget curl nmap net-tools ncdu btop openjdk-17-jdk vlc pkg-config -y
-
-#pra codar
-sudo apt install build-essential gcc vim git cmake zsh -y
-
-
-
-sudo apt purge imagemagick imagemagick-6-common -y
-sudo apt autoremove -y  && sudo apt autoclean
 
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/
 sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/sudo ln -s ~/.local/kitty.app/bin/kitty /usr/local/bin/n
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Ubuntu.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/UbuntuMono.zip
 
